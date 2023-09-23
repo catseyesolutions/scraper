db078e3ba3829baa0984c83413f09dc3dc0afa4b
catseyesolutions <smyrna75@gmail.com>
2023-09-23 12:38:22 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require('express'); // Adding Express
 const app = express(); // Initializing Express
+const locateChrome = require('locate-chrome');
+const executablePath = await new Promise(resolve => locateChrome(arg => resolve(arg)));
+
 const puppeteer = require('puppeteer'); // Adding Puppeteer
 
     app.get('/api', function(req, res) {
 
     // Launching puppeteer
-    puppeteer.launch({executablePath: '/opt/render/.cache/puppeteer/chrome', headless: true, args: ["--no-sandbox", "--disable-setuid-sandbox"]}).then(async function(browser) {
+    puppeteer.launch({executablePath, headless: true, args: ["--no-sandbox", "--disable-setuid-sandbox"]}).then(async function(browser) {
         const page = await browser.newPage();
 
         await page.goto(req.query.url, { waitUntil: 'networkidle0' })
